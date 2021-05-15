c3b8389772d1b18094e34504692fe620ef752bbb
Yaser <62748315+YaserTheDev@users.noreply.github.com>
2021-05-15 12:45:59 +0530

    Update rts.c

--- rts.c
+++ rts.c
@@ -1,51 +1,33 @@
-//DOUBT
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-struct node{
-    int data;
-    struct node* next;
-};
-struct node* top;
-void push(int n){
-    struct node* temp = (struct node*)malloc(sizeof(struct node));
-    temp->data = n;
-    temp->next = top;
-    top = temp;
-}
-int Top(){
-    return top;
+#define max 20
+
+int top = -1;
+char stack[max];
+
+void push(char s){
+    top++;
+    stack[top]=s;
 }
 void pop(){
-    struct node* temp;
-    if(top==NULL) return;
-    temp = top;
-    top = top->next;
-    free(temp);
+    char y;
+    y=stack[top];
+    top--;
+    printf("%c",y);
 }
-void reverse(char *c,int n){
-    for(int i=0;i<n;i++){
-        push(c[i]);
+int main()
+{
+    char str[max];
+    int i,len;
+    printf("Enter the string: ");
+    scanf("%c",str);
+    len = strlen(str);
+    for(i=0;i<len;i++){
+        push(str[i]);
     }
-    for(int i=0;i<n;i++){
-        c[i] = Top();
+    printf("Reversed string: ");
+    for(i=0;i<len;i++){
         pop();
     }
-}
-void print(){
-    struct node* temp = top;
-    printf("Stack is ");
-    while(temp!=NULL){
-        printf("%d ",temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-int main(){
-    top = NULL;
-    char c[50];
-    printf("Enter the string: ");
-    scanf("%s",&c);
-    reverse(c,strlen(c));
-    printf("Output: %s",c);
+    return 0;
 }
