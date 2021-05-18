0d356d8d22fc536ebb5e3d3192f87e153034920a
Yaser <62748315+YaserTheDev@users.noreply.github.com>
2021-05-18 12:31:55 +0530

    Update sull.c

--- sull.c
+++ sull.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node* next;
+};
+struct node* top;
+void push(int x){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data=x;
+    temp->next=top;
+    top=temp;
+}
+void pop(){
+    struct node* temp = top;
+    if(temp==NULL) return;
+    top=temp->next;
+    free(temp);
+}
+void display(){
+    struct node* temp = top;
+    printf("Stack is ");
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    top = NULL;
+    int n,x;
+    printf("How many numbers want to insert: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        printf("Enter the number: ");
+        scanf("%d",&x);
+        push(x);
+        display();
+    }
+    pop();display();
+    pop();display();
+    return 0;
+}
 
